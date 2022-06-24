6b930ba044c15391b1188155226ff115710cec7f
Bom <11377ny@gmail.com>
2022-06-24 15:01:02 -0400

    Finish skeleton of components

--- src/components/AddFood.js
+++ src/components/AddFood.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const AddFood = () => {
     return (
         <div>
             <h3>Add Food</h3>
+            <Link to="/:id/foods">
+                <button>Submit</button>
+            </Link>
+            <Link to=":id/foods">
+                <button>Back</button>
+            </Link>
         </div>
     )
 }
--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Food from "./Food";
 
 const FoodList = () => {
     return (
         <div>
             <h2>Food List</h2>
+            <Link to="/:id/add">
+                <button>Add New Food</button>
+            </Link>
+            <Food />
         </div>
     )
 }
