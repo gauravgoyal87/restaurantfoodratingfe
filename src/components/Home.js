63f3be0bf8909d565be49f221a7c2194ba08f0ba
Quoc Lam <LamQuoc.24@gmail.com>
2022-06-27 15:35:43 -0400

    Home Page Button CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <p>I am a landing page</p>
-        </div>
-    )
-}
+  return (
+    <div className="home-parent">
+      <p>
+        "UH... WHAT WAS THAT
+        <span>THING WE ATE</span>
+        THAT ONE TIME?"
+      </p>
+      <p>... YOU MIGHT WANNA WRITE THAT DOWN</p>
+//!Add Link (Quocky)
+      <button>WRITE IT DOWN</button>
 
-export default Home
+    </div>
+  );
+};
+
+export default Home;
