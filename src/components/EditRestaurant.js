19b217478a6abab52eefe5723648f1402190c2f4
Bom <11377ny@gmail.com>
2022-06-27 14:22:58 -0400

    Add edit restaurant form

--- src/components/EditRestaurant.js
+++ src/components/EditRestaurant.js
@@ -1,9 +1,53 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const EditRestaurant = () => {
+
+    const [form, setForm] = useState({});
+
+    const handleName = (e) => {
+      setForm({
+        name: e.target.value,
+        price: form.price,
+        review: form.review,
+      });
+    }
+
+    const handlePrice = (e) => {
+        setForm({
+          name: form.menuName,
+          price: e.target.value,
+          review: form.review,
+        });
+    }
+    
+    const handleReview = (e) => {
+        setForm({
+          name: form.menuName,
+          price: form.price,
+          review: e.target.value,
+        });
+    }
+
+    const handleSubmit = () => {
+        console.log(form)
+    }
+
     return (
         <div>
             <h3>Edit Restaurant</h3>
+            <form action=''>
+                <input type='text' placeholder='Name' id='menuName' onChange={handleName} />
+                <input type='text' placeholder='Price' id='price' onChange={handlePrice} />
+                <input type='text' placeholder='Review' id='review' onChange={handleReview} />
+                <Link to='/restaurants'>
+                    <button onClick={handleSubmit}>Submit</button>
+                </Link>
+            </form>
+            <Link to="/restaurants">
+                <button>Back</button>
+            </Link>
         </div>
     )
 }
