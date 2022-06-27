37168c19ce7059457bafce644f20a107790c582e
Quoc Lam <LamQuoc.24@gmail.com>
2022-06-27 15:10:14 -0400

    Footer Css

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import AddFood from './components/AddFood';
-import AddRestaurant from './components/AddRestaurant';
-import EditFood from './components/EditFood';
-import EditRestaurant from './components/EditRestaurant';
-import Food from './components/Food';
-import FoodList from './components/FoodList';
-import Home from './components/Home';
-import Nav from './components/Nav';
-import Restaurant from './components/Restaurant';
-import RestaurantList from './components/RestaurantList';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import AddFood from "./components/AddFood";
+import AddRestaurant from "./components/AddRestaurant";
+import EditFood from "./components/EditFood";
+import EditRestaurant from "./components/EditRestaurant";
+import Food from "./components/Food";
+import FoodList from "./components/FoodList";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Restaurant from "./components/Restaurant";
+import RestaurantList from "./components/RestaurantList";
 
 const App = () => {
-
   const [rest, setRes] = useState([]);
-  
+
   useEffect(() => {
-    fetch('./data.json')
+    fetch("./data.json")
       .then((response) => response.json())
       .then((data) => {
-        setRes(data)
+        setRes(data);
       })
       .catch((error) => {
-        console.log(error)
-      })
-  }, [])
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="App">
       <Nav />
       <main>
         <Routes>
-          <Route path="/"element={<Home />} />
-          <Route path="/restaurants" element={<RestaurantList restaurants={rest} />} />
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/restaurants"
+            element={<RestaurantList restaurants={rest} />}
+          />
           <Route path="/restaurants/add" element={<AddRestaurant />} />
           <Route path="/:id" element={<Restaurant restaurants={rest} />} />
           <Route path="/:id/edit" element={<EditRestaurant />} />
@@ -43,9 +45,14 @@ const App = () => {
           <Route path="/:id/:foodId" element={<Food />} />
           <Route path="/:id/:foodId/edit" element={<EditFood />} />
         </Routes>
+<div className="footer">
+        <img src="imgs/VectorLeft.png" alt="" />
+        <img src="imgs/VectorRight.png" alt="" />
+</div>
+
       </main>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
     return (
         <div>
-            <h2>This is an app to collect your review of restaurant's menu</h2>
+            <p>I am a landing page</p>
         </div>
     )
 }
