ef92bd4664beffc7ff025218b91323198450b1c8
Quoc Lam <LamQuoc.24@gmail.com>
2022-06-28 15:22:51 -0400

    Add Restaurant Text Edit

--- src/components/AddRestaurant.js
+++ src/components/AddRestaurant.js
@@ -3,42 +3,51 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const AddRestaurant = () => {
+  const [form, setForm] = useState({});
 
-    const [form, setForm] = useState({});
+  const handleName = (e) => {
+    setForm({
+      restaurantName: e.target.value,
+      location: form.location,
+    });
+  };
 
-    const handleName = (e) => {
-        setForm({
-            restaurantName: e.target.value,
-            location: form.location
-      });
-    }
+  const handleLocation = (e) => {
+    setForm({
+      restaurantName: form.restaurantName,
+      location: e.target.value,
+    });
+  };
 
-    const handleLocation = (e) => {
-        setForm({
-            restaurantName: form.restaurantName,
-            location: e.target.value
-        });
-    }
+  const handleSubmit = () => {
+    console.log(form);
+  };
 
-    const handleSubmit = () => {
-        console.log(form)
-    }
+  return (
+    <div className="ar-parent">
+      <h3>Add Restaurant</h3>
+      <form action="">
+        <input
+          type="text"
+          placeholder="Name"
+          id="restaurantName"
+          onChange={handleName}
+        />
+        <input
+          type="text"
+          placeholder="Location"
+          id="location"
+          onChange={handleLocation}
+        />
+        <Link to="/restaurants">
+          <button onClick={handleSubmit}>Submit</button>
+        </Link>
+      </form>
+      <Link to="/restaurants">
+        <button>Back</button>
+      </Link>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <h3>Add Food</h3>
-            <form action="">
-                <input type="text" placeholder="Name" id="restaurantName" onChange={handleName} />
-                <input type="text" placeholder="Location" id="location" onChange={handleLocation} />
-                <Link to="/restaurants">
-                    <button onClick={handleSubmit}>Submit</button>
-                </Link>
-            </form>
-            <Link to="/restaurants">
-                <button>Back</button>
-            </Link>
-        </div>
-    )
-}
-
-export default AddRestaurant
+export default AddRestaurant;
