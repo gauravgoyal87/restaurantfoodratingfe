1b76e0a985749b203de90e6e01f54a139bf2f0bb
Quoc Lam <LamQuoc.24@gmail.com>
2022-06-28 10:02:49 -0400

    Home Page CSS Complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,16 +2,23 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="home-parent">
-      <p>
-        "UH... WHAT WAS THAT
-        <span>THING WE ATE</span>
-        THAT ONE TIME?"
-      </p>
-      <p>... YOU MIGHT WANNA WRITE THAT DOWN</p>
-//!Add Link (Quocky)
-      <button>WRITE IT DOWN</button>
+    <div className="home-great-grandparent">
+      <div className="home-uncle">
+        <img src="imgs/Spoon.png" alt="" />
+        <img className="home-grease" src="imgs/Grease.png" alt="" />
+      </div>
+        <div className="home-parent">
+          <p>
+            "UH... WHAT WAS THAT
+            <span> THING WE ATE </span>
+            THAT ONE TIME?"
+          </p>
+          <p>
+            ... YOU MIGHT WANNA <span>WRITE</span> THAT SOMEWHERE
+          </p>
 
+          <button>WRITE IT DOWN</button>
+        </div>
     </div>
   );
 };
