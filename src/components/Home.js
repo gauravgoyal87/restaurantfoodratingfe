4a212ae7f96cb430660d59a9e0494b95e8464b3a
Quoc Lam <LamQuoc.24@gmail.com>
2022-06-28 10:18:51 -0400

    Fixed home page Link Css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -7,18 +8,19 @@ const Home = () => {
         <img src="imgs/Spoon.png" alt="" />
         <img className="home-grease" src="imgs/Grease.png" alt="" />
       </div>
-        <div className="home-parent">
-          <p>
-            "UH... WHAT WAS THAT
-            <span> THING WE ATE </span>
-            THAT ONE TIME?"
-          </p>
-          <p>
-            ... YOU MIGHT WANNA <span>WRITE</span> THAT SOMEWHERE
-          </p>
-
-          <button>WRITE IT DOWN</button>
-        </div>
+      <div className="home-parent">
+        <p className="home-p1" >
+          "UH... WHAT WAS THAT
+          <span> THING WE ATE </span>
+          THAT ONE TIME?"
+        </p>
+        <p>
+          ... YOU MIGHT WANNA <span className="home-write" >WRITE</span> THAT SOMEWHERE
+        </p>
+        <Link to="/restaurants">
+          <button className="home-button" >WRITE IT DOWN</button>
+        </Link>
+      </div>
     </div>
   );
 };
