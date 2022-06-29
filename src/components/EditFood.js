a0b4b6b78e20c5615c738969653eb5783e983e66
Quoc Lam <LamQuoc.24@gmail.com>
2022-06-29 12:32:04 -0400

    Edit Food

--- src/components/EditFood.js
+++ src/components/EditFood.js
@@ -4,82 +4,103 @@ import { useState } from "react";
 import axios from "axios";
 
 const EditFood = () => {
+  const restName = useParams().restName;
+  const foodName = useParams().foodName;
 
-    const restName = useParams().restName
-    const foodName = useParams().foodName
+  const [form, setForm] = useState({
+    image: "",
+    dish: "",
+    comment: "",
+    rating: "",
+    restaurant: restName,
+  });
 
-    const [form, setForm] = useState({
-        image: '',
-        dish: '',
-        comment: '',
-        rating: '',
-        restaurant: restName
+  const handleImage = (e) => {
+    setForm({
+      image: e.target.value,
+      dish: form.dish,
+      comment: form.comment,
+      rating: form.rating,
+      restaurant: restName,
     });
+  };
 
-    const handleImage = (e) => {
-        setForm({
-            image: e.target.value,
-            dish: form.dish,
-            comment: form.comment,
-            rating: form.rating,
-            restaurant: restName
-        });
-      }
-  
-      const handleDish = (e) => {
-          setForm({
-            image: form.image,
-            dish: e.target.value,
-            comment: form.comment,
-            rating: form.rating,
-            restaurant: restName
-          });
-      }
-      
-      const handleComment = (e) => {
-          setForm({
-            image: form.image,
-            dish: form.dish,
-            comment: e.target.value,
-            rating: form.rating,
-            restaurant: restName
-          });
-      }
+  const handleDish = (e) => {
+    setForm({
+      image: form.image,
+      dish: e.target.value,
+      comment: form.comment,
+      rating: form.rating,
+      restaurant: restName,
+    });
+  };
 
-      const handleRating = (e) => {
-        setForm({
-          image: form.image,
-          dish: form.dish,
-          comment: form.comment,
-          rating: e.target.value,
-          restaurant: restName
-        });
-    }
-  
-    const handleSubmit = () => {
-        axios.put(`http://localhost:4040/api/food/${foodName}`, form)
-        .then(res => {
-            console.log(res.data)
-        })
-    }
-  
-      return (
-          <div>
-              <h3>Edit Restaurant</h3>
-              <form action=''>
-                  <input type='text' placeholder='Image' id='image' onChange={handleImage} />
-                  <input type='text' placeholder='Dish' id='dish' onChange={handleDish} />
-                  <input type='text' placeholder='Comment' id='comment' onChange={handleComment} />
-                  <input type='number' placeholder='Rating' id='rating' onChange={handleRating} />
-                  <Link to={`/${restName}`}>
-                      <button onClick={handleSubmit}>Submit</button>
-                  </Link>
-              </form>
-              <Link to={`/${restName}`}>
-                  <button>Back</button>
-              </Link>
-          </div>
-      )
-  }
+  const handleComment = (e) => {
+    setForm({
+      image: form.image,
+      dish: form.dish,
+      comment: e.target.value,
+      rating: form.rating,
+      restaurant: restName,
+    });
+  };
 
-export default EditFood
+  const handleRating = (e) => {
+    setForm({
+      image: form.image,
+      dish: form.dish,
+      comment: form.comment,
+      rating: e.target.value,
+      restaurant: restName,
+    });
+  };
+
+  const handleSubmit = () => {
+    axios
+      .put(`http://localhost:4040/api/food/${foodName}`, form)
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
+  return (
+    <div className="ef-parent">
+      <div className="ef-title">
+        <p>Edit Food</p>
+      </div>
+
+      <form className="ef-form" action="">
+        <input
+          type="text"
+          placeholder="Image"
+          id="image"
+          onChange={handleImage}
+        />
+        <input type="text" placeholder="Dish" id="dish" onChange={handleDish} />
+        <input
+          type="text"
+          placeholder="Comment"
+          id="comment"
+          onChange={handleComment}
+        />
+        <input
+          type="number"
+          placeholder="Rating"
+          id="rating"
+          onChange={handleRating}
+        />
+
+        <div className="ef-buttons">
+          <Link  to={`/${restName}`}>
+            <button className="af-cancel" >Back</button>
+          </Link>
+          <Link to={`/${restName}`}>
+            <button className="af-next" onClick={handleSubmit}>Submit</button>
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default EditFood;
